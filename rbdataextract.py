16b965ca50e7748ed1a1500632d9d96a34f63db1
devhicks210 <jmatthicks+1@gmail.com>
2016-04-02 23:47:09 -0700

    The ACTUAL code used to hit the API, put in SQL
    Here's the code I use to go, week by week, for each year, to hit every API endpoint for each running back and then post the data back into my SQL database.

--- rbdataextract.py
+++ rbdataextract.py
@@ -3,17 +3,18 @@
 import json
 import pymysql
 import requests
+import re
 
 # API information
 
-endpoint = "http://www.fantasyfootballnerd.com/service/player/json/"    # plus KEY, plus PLAYERID
+endpoint = "http://www.fantasyfootballnerd.com/service/player/json/"    # plus KEY plus PLAYERID
 key = 'XXXXX'
 
 # Accessing SQL DB and looking into desired table
 
 conn = pymysql.connect(host='localhost', port=XXXXX, user='XXXXX', passwd='XXXXX', db='XXXXX')
 cur = conn.cursor()
-cur.execute("""SELECT player_id FROM rblist;""")
+cur.execute("""SELECT id FROM allBacks;""")
 rows = cur.fetchall()
 
 # Creating a holder for all running back IDs, then filling it with said IDs
@@ -31,19 +32,6 @@ rb_endpoints = []
 for x in rb_ids:
 	rb_endpoints.append(str(endpoint + key + x + "/"))
 
-# Defining functions to get the past 10 years worth of data for each of the 17 weeks of the NFL regular season
-
-def getYear():
-	for x in range(2005, 2016):
-		yield str(x)
-
-def getWeek():
-	for x in range(1, 18):
-		yield str(x)
-
-years = getYear()
-weeks = getWeek()
-
 # Iterating through each API endpoint, hitting it, and getting the necessary data back.
 
 for x in rb_endpoints:
@@ -53,20 +41,86 @@ for x in rb_endpoints:
 	statskey = data['Stats']
 
 	try:
-		rush_att = int(statskey['2015']['1']['rushAttempts'])
-		rush_yards = int(statskey['2015']['1']['rushYards'])
-		rush_td = int(statskey['2015']['1']['rushTD'])
-		receptions = int(statskey['2015']['1']['receptions'])
-		rec_yards = int(statskey['2015']['1']['recYards'])
-		rec_td = int(statskey['2015']['1']['recTD'])
-		fum_lost = int(statskey['2015']['1']['fumbleLost'])
+		if "'" in data['Player']['fname']:
+			fname = re.sub("'", "", data['Player']['fname'])
+			lname = data['Player']['lname']
+			playerId = data['Player']['playerId']
+			team = data['Player']['team']
 
-		cur.execute("""INSERT INTO season_2015 (carries, rushyds, runtd, rec, recyds, rectd, fum)
-				VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s');""" % (rush_att, rush_yards, rush_td, receptions, rec_yards, rec_td, fum_lost))
-		conn.commit()
+			# This below info is for calculating age...unfortunately, the API's birthday data is not accurate.
+			# dob1 = data['Player']['dob']
+			# dob3 = ''
+			# dob2 = dob1.split('-')
+			# d0 = date(int(dob2[0]), int(dob2[1]), int(dob2[2]))
+			# d1 = date(2016, 4, 2)
+			# delta = d1 - d0
+
+			# for x in dob2:
+			# 	dob3 = dob3 + x + ":"
+
+			rush_att = int(statskey['2014']['8']['rushAttempts'])
+			rush_yards = int(statskey['2014']['8']['rushYards'])
+			rush_td = int(statskey['2014']['8']['rushTD'])
+			receptions = int(statskey['2014']['8']['receptions'])
+			rec_yards = int(statskey['2014']['8']['recYards'])
+			rec_td = int(statskey['2014']['8']['recTD'])
+			fum_lost = int(statskey['2014']['8']['fumbleLost'])
+			week = int(statskey['2014']['8']['week'])
+			opp = statskey['2014']['8']['opponent']
+			# dob4 = dob3[:-1]
+
+			cur.execute("""INSERT INTO season_2014 (opp, week, team, fname, lname, rb_id, carries, rushyds, runtd, rec, recyds, rectd, fum)
+					VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');""" % (opp, week, team, fname, lname, playerId, rush_att, rush_yards, rush_td, receptions, rec_yards, rec_td, fum_lost))
+			conn.commit()
+
+			print playerId, fname, lname, "SUCCESS!"
+		
+		elif "'" in data['Player']['lname']:
+			fname = data['Player']['fname']
+			lname = re.sub("'", "", data['Player']['lname'])
+			playerId = data['Player']['playerId']
+			team = data['Player']['team']
+
+			rush_att = int(statskey['2014']['8']['rushAttempts'])
+			rush_yards = int(statskey['2014']['8']['rushYards'])
+			rush_td = int(statskey['2014']['8']['rushTD'])
+			receptions = int(statskey['2014']['8']['receptions'])
+			rec_yards = int(statskey['2014']['8']['recYards'])
+			rec_td = int(statskey['2014']['8']['recTD'])
+			fum_lost = int(statskey['2014']['8']['fumbleLost'])
+			week = int(statskey['2014']['8']['week'])
+			opp = statskey['2014']['8']['opponent']
+
+			cur.execute("""INSERT INTO season_2014 (opp, week, team, fname, lname, rb_id, carries, rushyds, runtd, rec, recyds, rectd, fum)
+					VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');""" % (opp, week, team, fname, lname, playerId, rush_att, rush_yards, rush_td, receptions, rec_yards, rec_td, fum_lost))
+			conn.commit()
+
+			print playerId, fname, lname, "SUCCESS!"
+
+		else:
+			fname = data['Player']['fname']
+			lname = data['Player']['lname']
+			playerId = data['Player']['playerId']
+			team = data['Player']['team']
+
+			rush_att = int(statskey['2014']['8']['rushAttempts'])
+			rush_yards = int(statskey['2014']['8']['rushYards'])
+			rush_td = int(statskey['2014']['8']['rushTD'])
+			receptions = int(statskey['2014']['8']['receptions'])
+			rec_yards = int(statskey['2014']['8']['recYards'])
+			rec_td = int(statskey['2014']['8']['recTD'])
+			fum_lost = int(statskey['2014']['8']['fumbleLost'])
+			week = int(statskey['2014']['8']['week'])
+			opp = statskey['2014']['8']['opponent']
+
+			cur.execute("""INSERT INTO season_2014 (opp, week, team, fname, lname, rb_id, carries, rushyds, runtd, rec, recyds, rectd, fum)
+					VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');""" % (opp, week, team, fname, lname, playerId, rush_att, rush_yards, rush_td, receptions, rec_yards, rec_td, fum_lost))
+			conn.commit()
+
+			print playerId, fname, lname, "SUCCESS!"
 
 	except:
-		pass
+		print "NO STATS"
 
 cur.close()
 conn.close()
