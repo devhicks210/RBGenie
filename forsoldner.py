bd662f964c139186ac36f6185b3aa76de0abb08f
devhicks210 <jmatthicks+1@gmail.com>
2016-04-07 12:52:20 -0700

    Update forsoldner.py

--- forsoldner.py
+++ forsoldner.py
@@ -1,7 +1,9 @@
-def recyds(self, year):
+class idealRB1(object):
 
-		cur.execute("""SELECT recyds, fname, lname FROM season_%s_totals ORDER BY recyds DESC LIMIT 10;""", (year))
-		rows = cur.fetchall()
-
-		for x, y, z in rows:
-			print y, z, x
+	def recyds(self, year):
+	
+			cur.execute("""SELECT recyds, fname, lname FROM season_%s_totals ORDER BY recyds DESC LIMIT 10;""", (year))
+			rows = cur.fetchall()
+	
+			for x, y, z in rows:
+				print y, z, x
